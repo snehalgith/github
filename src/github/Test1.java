ddd868b7520ca81148cf138272c7ae1071d2b04b
HP <HP@DESKTOP-5NRC6GA>
2026-05-28 10:33:03 +0530

    First Commit...

---
+++ src/github/Test1.java
@@ -0,0 +1,15 @@
+package github;
+
+public class Test1 {
+    public static void main(String[] args) {
+    	int a=19;
+		if(a==10)
+		{
+			System.out.println("hii");
+		}
+		else
+		{
+			System.out.println("byee");
+		}
+	}
+}
