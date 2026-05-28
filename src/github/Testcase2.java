a5e98c81d49f68c50dabf4a17633c4d145665fe6
HP <HP@DESKTOP-5NRC6GA>
2026-05-28 19:18:05 +0530

    please again review

--- src/github/Testcase2.java
+++ src/github/Testcase2.java
@@ -5,7 +5,7 @@ public class Testcase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int b=8;
-        if(b==8)
+        if(b==8||b==9)
         {
         	System.out.println("jk");
         }
